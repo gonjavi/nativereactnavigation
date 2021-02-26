7e8645faf6aefd3dc5ddc9b88ca460677f081d0e
manci <gmancillapaz@gmail.com>
2021-02-26 15:51:40 -0500

    crear navegacion y componentes

--- App.js
+++ App.js
@@ -9,12 +9,28 @@ import {
 // react navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Inicio from './views/Inicio';
+import Nosotros from './views/Nosotros';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
      <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Inicio"
+      >
+        <Stack.Screen
+          name='Inicio'
+          component={Inicio}
+        />
+        <Stack.Screen
+          name='Nosotros'
+          component={Nosotros}
+        />
 
+      </Stack.Navigator>
      </NavigationContainer>
     </>
   );
